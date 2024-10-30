8e1a147eabfd649766caa31ea521682ecb470b93
Ivan <128108289+IdikiiD@users.noreply.github.com>
2024-10-30 10:46:00 +0300

    Updated description , added label 'There is no description about hero'

--- src/server/Server.js
+++ src/server/Server.js
@@ -24,7 +24,7 @@ class MarvelService {
     _transformCaracter = (char)=>{
         return{
             name:char.name,
-            description:char.description,
+            description:char.description ? ` ${char.description.slice(0,210)}...`: 'There is no description about hero',
             thumbnail:char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage:char.urls[0].url,
             wiki:char.urls[1].url
